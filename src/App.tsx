41916690a0549298a4e7f2bf1daf5d6cdc6c497c
Junior Garcia <47872989+garciajuni20@users.noreply.github.com>
2026-01-30 15:22:56 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,18 @@ import Home from "./pages/Home"
 import ResumePage from "./pages/Resume"
 import Projects from "./pages/Projects"
 import Contact from "./pages/Contact"
-import { LanguageProvider } from "./context/LanguageContext"
-import { ThemeProvider } from "./context/ThemeContext"
 
 export default function App() {
   return (
-    <ThemeProvider>
-    <LanguageProvider>
-      <HashRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<ResumePage />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </HashRouter>
-    </LanguageProvider>
-    </ThemeProvider>
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<ResumePage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
   )
 }
