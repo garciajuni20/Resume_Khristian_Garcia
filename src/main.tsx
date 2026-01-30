0c1b47d234407af363e3cc60735225386b85c03e
Junior Garcia <47872989+garciajuni20@users.noreply.github.com>
2026-01-30 14:32:07 -0600

    Add context providers around App component
    Wrap App component with ThemeProvider and LanguageProvider.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
+import App from "./App"
 import "./index.css"
+
+import { LanguageProvider } from "./context/LanguageContext"
+import { ThemeProvider } from "./context/ThemeContext"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
