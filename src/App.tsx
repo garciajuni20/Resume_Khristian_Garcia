484ab3b8e82dca0bb1bc66c6a78bffe98ed0f71c
Junior Garcia <47872989+garciajuni20@users.noreply.github.com>
2026-02-03 21:14:11 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from './context/LanguageContext';
-import { ThemeProvider } from './context/ThemeContext';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Resume from './pages/Resume';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "./context/LanguageContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
-// Script de Google Analytics (opcional)
-const GA_TRACKING_ID = 'G-XXXXXXXXXX'; // Reemplaza con tu ID
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function App() {
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
   return (
-    <Router>
-      <ThemeProvider>
-        <LanguageProvider>
-          <div className="min-h-screen flex flex-col">
+    <ThemeProvider>
+      <LanguageProvider>
+        <HashRouter>
+          <div className="min-h-screen bg-black text-white">
             <Navbar />
-            <main className="flex-1">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/resume" element={<Resume />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
             <Footer />
           </div>
-        </LanguageProvider>
-      </ThemeProvider>
-    </Router>
+        </HashRouter>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
-
-export default App;
